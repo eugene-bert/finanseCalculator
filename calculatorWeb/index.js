b7a4830b6a33f6bc8b756f7a7023d4ebd8fb1d38
evgenybert <evgenybertsovich@gmail.com>
2020-07-04 12:37:58 +0600

    - financeCalculator.csv file added - calculator html css draft

--- calculatorWeb/index.js
+++ calculatorWeb/index.js
@@ -1,12 +1,12 @@
-console.log('test')
-
 $(document).ready(function(){
     $.ajax({
         url:"http://localhost:5000/financeCalculator",
         dataType:"text",
         success:function(data)
         {
-            console.log(data)
+            let cdnData = data;
+            let results = Papa.parse(cdnData);
+            console.log(results)
         }
     });
 });
